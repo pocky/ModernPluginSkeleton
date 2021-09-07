8519dcdc00af32edfc03eed7ed5394778ea6abe9
Alexandre Balmes <alexandre@vanoix.com>
2021-09-07 17:15:28 +0200

    Fix path for assets, improve build

--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 const syliusBundles = path.resolve(__dirname, '../../vendor/sylius/sylius/src/Sylius/Bundle/');
 const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
 const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
